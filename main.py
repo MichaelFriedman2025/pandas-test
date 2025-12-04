a4816dac77b1a3d2e3d2ec6a2204e71ff092684b
michael friedman <myklp1013@gmail.com>
2025-12-04 11:58:41 +0200

    adding types for all function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils import load_json_into_df,changing_column_types,clearing_an_HTML_column,clearing_an_coupon_used_column,adding_column_order_month,adding_column_high_value_order,adding_column_rating,order_filtering,delivery_status,save_to_CSV_file
 
-def main():
+def main() -> None:
     df = load_json_into_df()
     df = changing_column_types(df)
     df = clearing_an_HTML_column(df)
--- utils.py
+++ utils.py
@@ -1,75 +1,66 @@
 import pandas as pd
 
 #level 0
-def load_json_into_df():
+def load_json_into_df() -> pd.DataFrame:
     df_read = pd.read_json(r"C:\Users\LENOVO\Desktop\pandas_tast\orders_simple.json")
     return df_read
 
-
 #level 1
-def clean_column_total_amount(data):
+def clean_column_total_amount(data:pd.DataFrame) -> pd.DataFrame:
     data["total_amount"] = data["total_amount"].str.replace("$","")
     return data
 
-def replacing_existing_columns(data,column,type_to):
-    if type_to == "float":
-        data[column] = data[column].astype(float)
-    elif type_to == "int":
-        data[column] = pd.to_numeric(data[column])
-    elif type_to == "datetime":
-        data[column] = pd.to_datetime(data[column])
-    return data
-
-def changing_column_types(data):
+def changing_column_types(data:pd.DataFrame) -> pd.DataFrame:
     data = clean_column_total_amount(data)
-    data = replacing_existing_columns(data,"total_amount","float")
-    data = replacing_existing_columns(data,"shipping_days","int")
-    data = replacing_existing_columns(data,"customer_age","int")
-    data = replacing_existing_columns(data,"rating","float")
-    data = replacing_existing_columns(data,"order_date","datetime")
+    # type change to float
+    data["total_amount"] = data["total_amount"].astype(float)
+    data["rating"] = data["rating"].astype(float)
+    # type change to int
+    data["shipping_days"] = pd.to_numeric(data["shipping_days"])
+    data["customer_age"] = pd.to_numeric(data["customer_age"])
+    # type change to datetime
+    data["order_date"] = pd.to_datetime(data["order_date"])
     return data
 
 #level 2
-def clearing_an_HTML_column(data):
+def clearing_an_HTML_column(data:pd.DataFrame) -> pd.DataFrame: 
     data['items_html'] = data['items_html'].str.replace("<.*?>",' ',regex=True)
     return data
 
 #level 3
-def clearing_an_coupon_used_column(data):
+def clearing_an_coupon_used_column(data:pd.DataFrame) -> pd.DataFrame:
     data["coupon_used"] = data["coupon_used"].str.replace("","no_coupon")
     return data
 
 #level 4
-def adding_column_order_month(data):
+def adding_column_order_month(data:pd.DataFrame) -> pd.DataFrame:
     data["order_month"] = data["order_date"].dt.month
     return data
 
 #level 5
-def adding_column_high_value_order(data):
+def adding_column_high_value_order(data:pd.DataFrame) -> pd.DataFrame:
     avg_total_amount = data["total_amount"].mean()
     data["high_value_order"] = data["total_amount"] > avg_total_amount
     sort_data = data.sort_values("total_amount",ascending=False)
     return sort_data
 
 #level 6
-def adding_column_rating(data:pd.DataFrame):
+def adding_column_rating(data:pd.DataFrame) -> pd.DataFrame:
     avg_rating = data.groupby("country")["rating"].mean()
     data["avg_rating_per_country"] = avg_rating[data["country"]].values
     return data
 
 #level 7
-def order_filtering(data):
-    df = data[(data["total_amount"] > 1000) & (data["rating"] > 4.5)]
-    return df
+def order_filtering(data:pd.DataFrame) -> pd.DataFrame:
+    data = data[(data["total_amount"] > 1000) & (data["rating"] > 4.5)]
+    return data
 
 #level 8
-def delivery_status(data):
+def delivery_status(data:pd.DataFrame) -> pd.DataFrame:
     data["delivery_status"] = data["shipping_days"].apply(lambda x: "delayed" if x > 7 else "on_time")
     return data
 
 #level 9
-def save_to_CSV_file(data):
+def save_to_CSV_file(data:pd.DataFrame) -> None:
     data.to_csv(r"C:\Users\LENOVO\Desktop\pandas_tast\clean_order_[213289390].csv",index=False)
     return
-
-
