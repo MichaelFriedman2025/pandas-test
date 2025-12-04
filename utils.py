a315a8ea055a862ca8b61ab267dd6ddb1dba34a7
michael friedman <myklp1013@gmail.com>
2025-12-04 12:08:37 +0200

    chchanging the path in function

--- utils.py
+++ utils.py
@@ -1,16 +1,19 @@
 import pandas as pd
 
-#level 0
+
+# level 0
 def load_json_into_df() -> pd.DataFrame:
-    df_read = pd.read_json(r"C:\Users\LENOVO\Desktop\pandas_tast\orders_simple.json")
+    df_read = pd.read_json("orders_simple.json")
     return df_read
 
-#level 1
-def clean_column_total_amount(data:pd.DataFrame) -> pd.DataFrame:
-    data["total_amount"] = data["total_amount"].str.replace("$","")
+
+# level 1
+def clean_column_total_amount(data: pd.DataFrame) -> pd.DataFrame:
+    data["total_amount"] = data["total_amount"].str.replace("$", "")
     return data
 
-def changing_column_types(data:pd.DataFrame) -> pd.DataFrame:
+
+def changing_column_types(data: pd.DataFrame) -> pd.DataFrame:
     data = clean_column_total_amount(data)
     # type change to float
     data["total_amount"] = data["total_amount"].astype(float)
@@ -22,45 +25,55 @@ def changing_column_types(data:pd.DataFrame) -> pd.DataFrame:
     data["order_date"] = pd.to_datetime(data["order_date"])
     return data
 
-#level 2
-def clearing_an_HTML_column(data:pd.DataFrame) -> pd.DataFrame: 
-    data['items_html'] = data['items_html'].str.replace("<.*?>",' ',regex=True)
+
+# level 2
+def clearing_an_HTML_column(data: pd.DataFrame) -> pd.DataFrame:
+    data["items_html"] = data["items_html"].str.replace("<.*?>", " ", regex=True)
     return data
 
-#level 3
-def clearing_an_coupon_used_column(data:pd.DataFrame) -> pd.DataFrame:
-    data["coupon_used"] = data["coupon_used"].str.replace("","no_coupon")
+
+# level 3
+def clearing_an_coupon_used_column(data: pd.DataFrame) -> pd.DataFrame:
+    data["coupon_used"] = data["coupon_used"].str.replace("", "no_coupon")
     return data
 
-#level 4
-def adding_column_order_month(data:pd.DataFrame) -> pd.DataFrame:
+
+# level 4
+def adding_column_order_month(data: pd.DataFrame) -> pd.DataFrame:
     data["order_month"] = data["order_date"].dt.month
     return data
 
-#level 5
-def adding_column_high_value_order(data:pd.DataFrame) -> pd.DataFrame:
+
+# level 5
+def adding_column_high_value_order(data: pd.DataFrame) -> pd.DataFrame:
     avg_total_amount = data["total_amount"].mean()
     data["high_value_order"] = data["total_amount"] > avg_total_amount
-    sort_data = data.sort_values("total_amount",ascending=False)
+    sort_data = data.sort_values("total_amount", ascending=False)
     return sort_data
 
-#level 6
-def adding_column_rating(data:pd.DataFrame) -> pd.DataFrame:
+
+# level 6
+def adding_column_rating(data: pd.DataFrame) -> pd.DataFrame:
     avg_rating = data.groupby("country")["rating"].mean()
     data["avg_rating_per_country"] = avg_rating[data["country"]].values
     return data
 
-#level 7
-def order_filtering(data:pd.DataFrame) -> pd.DataFrame:
+
+# level 7
+def order_filtering(data: pd.DataFrame) -> pd.DataFrame:
     data = data[(data["total_amount"] > 1000) & (data["rating"] > 4.5)]
     return data
 
-#level 8
-def delivery_status(data:pd.DataFrame) -> pd.DataFrame:
-    data["delivery_status"] = data["shipping_days"].apply(lambda x: "delayed" if x > 7 else "on_time")
+
+# level 8
+def delivery_status(data: pd.DataFrame) -> pd.DataFrame:
+    data["delivery_status"] = data["shipping_days"].apply(
+        lambda x: "delayed" if x > 7 else "on_time"
+    )
     return data
 
-#level 9
-def save_to_CSV_file(data:pd.DataFrame) -> None:
-    data.to_csv(r"C:\Users\LENOVO\Desktop\pandas_tast\clean_order_[213289390].csv",index=False)
+
+# level 9
+def save_to_CSV_file(data: pd.DataFrame) -> None:
+    data.to_csv("clean_order_213289390.csv", index=False)
     return
